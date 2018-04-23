ecc8b53ee4bd6d4008bf7e119fc1600f79ad0b5f
RichyST <a01337828@itesm.mx>
2018-04-23 16:47:42 -0500

    vale verga

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import {CuestionarioComponent} from './cuestionario/cuestionario.component';
+import { GraficasComponent } from './graficas/graficas.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CuestionarioComponent,
+    GraficasComponent,
+    CuestionarioComponent
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/cuestionario/cuestionario.component.ts
@@ -0,0 +1,80 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cuestionario',
+  templateUrl: './cuestionario.component.html',
+  styleUrls: ['./cuestionario.component.css']
+})
+export class CuestionarioComponent implements OnInit {
+  materias = [
+    { mat: "History" }, { mat: "Psychology" }, { mat: "Politics" }, { mat: "Mathematics" }, { mat: "Physics" },
+    { mat: "Computer" }, { mat: "Economy" }, { mat: "Biology" }, { mat: "Chemistry" }, { mat: "Reading" },
+    { mat: "Geography" }, { mat: "Foreign languages" }, { mat: "Medicine" }, { mat: "Law" }];
+  pasatiempos = [
+    { pas: "Art exhibitions" }, { pas: "Religion" }, { pas: "Countryside, outdoors" }, { pas: "Dancing" }
+    , { pas: "Musical instruments" }, { pas: "Writing" }, { pas: "Passive sport" }, { pas: "Active sport" }
+    , { pas: "Gardening" }, { pas: "Celebrities" }, { pas: "Celebrities" }, { pas: "Shopping" }
+    , { pas: "Science and technology" }, { pas: "Theatre" }, { pas: "Fun with friends" }, { pas: "Adrenaline sports" }
+    , { pas: "Pets" }];
+  miedos = [{ mie: "Flying" }, { mie: "Storm" }, { mie: "Darkness" }, { mie: "DHeights" },
+  { mie: "Spiders" }, { mie: "Snakes" }, { mie: "Rats" }, { mie: "Ageing" }, { mie: "Dangerous dogs" },
+  { mie: "Fear of public speaking" }];
+
+  frases = [{ fra: "Healthy eating" }, { fra: "Daily events" }, { fra: "Prioritising workload" }, { fra: "Writing notes" },
+    { fra: "Workaholism" }, { fra: "Thinking ahead" }, { fra: "Final judgement" }, { fra: "Reliability" }];
+
+  frases2 = [{ fra2: "Keeping promises" }, { fra2: "Loss of interest" }, { fra2: "Friends versus money" }, { fra2: "Funniness" },
+    { fra2: "Fake" }, { fra2: "Criminal damage" }, { fra2: "Decision making" }, { fra2: "Elections" }];
+
+  frases3 = [{ fra3: "Self-criticism" }, { fra3: "Judgement calls" }, { fra3: "Hypochondria" }, { fra3: "Empathy" },
+    { fra3: "Eating to survive" }, { fra3: "Giving" }, { fra3: "Compassion to animals" }, { fra3: "Borrowed stuff" }];
+
+  frases4 = [{ fra4: "Loneliness" }, { fra4: "Cheating in school" }, { fra4: "Health" }, { fra4: "Changing the past" },
+    { fra4: "God" }, { fra4: "Dreams" }, { fra4: "Charity" }, { fra4: "Number of friends" }];
+
+  importantes = [{ imp: "Waiting" }, { imp: "New environmentg" }, { imp: "Mood swings" }, { imp: "Appearance and gestures" }, { imp: "Socializing" }, 
+    { imp: "Achievements" }, { imp: "Responding to a serioues letter" }];
+
+  importantes2 = [{ imp: "Children" }, { imp: "Assertiveness" }, { imp: "Getting angry" }, { imp: "Knowing the right people" }, { imp: "Public speaking" },
+    { imp: "Unpopularity" }, { imp: "Life struggles" }];
+
+  importantes3 = [{ imp: "Happiness in life" }, { imp: "Energy levels" }, { imp: "Small dogs" }, { imp: "  Big dogs" }, { imp: "Personality" },
+    { imp: "Finding lost valuables" }, { imp: "Getting up" }, { imp: "Interests or hobbies" }, { imp: "Parent's advice" }, { imp: "Questionnaires or polls" }];
+  
+
+  cuest = {
+    numMusica1: 1,
+    musica2: 'Slow Song',
+    musica3: 'Pop',
+    musica4: 'Swing, Jazz',
+    numPeliculas: 1,
+    peliculas6: 'Horror',
+    peliculas7: 'War',
+    materiaFav8: [],
+    pasatiempo9: [],
+    miedo10: [],
+    beber11: 'never',
+    fumar12: 'never smoked',
+    frase13: [],
+    frase14: [],
+    frase15: [],
+    frase16: [],
+    puntual17: 'i am always on time',
+    mientes18: 'never',
+    importante19:[],
+    importante20:[],
+    importante21:[],
+    internet22:'less than an hour a day'
+  }
+
+  constructor() { }
+  
+  print() {
+    console.log(this.cuest);
+  }
+
+
+  ngOnInit() {
+  }
+
+}

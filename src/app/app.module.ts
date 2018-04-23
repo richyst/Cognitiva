931530fd154d5e0ba78dc6b52802aecb516d64ca
richyst <ricardo_1994@hotmail.com>
2018-04-23 18:23:29 -0500

    Feat: Formulario listo para enviado de datos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule, MatRadioModule, MatSelectModule, MatInputModule, MatFormFieldModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import {CuestionarioComponent} from './cuestionario/cuestionario.component';
@@ -14,11 +17,17 @@ import { GraficasComponent } from './graficas/graficas.component';
     CuestionarioComponent,
     GraficasComponent,
     CuestionarioComponent
-    
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    BrowserAnimationsModule,
+    MatRadioModule,
+    MatInputModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatButtonModule
+
 
   ],
   providers: [],
--- src/app/cuestionario/cuestionario.component.ts
+++ src/app/cuestionario/cuestionario.component.ts
@@ -7,40 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CuestionarioComponent implements OnInit {
   materias = [
-    { mat: "History" }, { mat: "Psychology" }, { mat: "Politics" }, { mat: "Mathematics" }, { mat: "Physics" },
-    { mat: "Computer" }, { mat: "Economy" }, { mat: "Biology" }, { mat: "Chemistry" }, { mat: "Reading" },
-    { mat: "Geography" }, { mat: "Foreign languages" }, { mat: "Medicine" }, { mat: "Law" }];
+    { mat: 'History' }, { mat: 'Psychology' }, { mat: 'Politics' }, { mat: 'Mathematics' }, { mat: 'Physics' },
+    { mat: 'Computer' }, { mat: 'Economy' }, { mat: 'Biology' }, { mat: 'Chemistry' }, { mat: 'Reading' },
+    { mat: 'Geography' }, { mat: 'Foreign languages' }, { mat: 'Medicine' }, { mat: 'Law' }];
   pasatiempos = [
-    { pas: "Art exhibitions" }, { pas: "Religion" }, { pas: "Countryside, outdoors" }, { pas: "Dancing" }
-    , { pas: "Musical instruments" }, { pas: "Writing" }, { pas: "Passive sport" }, { pas: "Active sport" }
-    , { pas: "Gardening" }, { pas: "Celebrities" }, { pas: "Celebrities" }, { pas: "Shopping" }
-    , { pas: "Science and technology" }, { pas: "Theatre" }, { pas: "Fun with friends" }, { pas: "Adrenaline sports" }
-    , { pas: "Pets" }];
-  miedos = [{ mie: "Flying" }, { mie: "Storm" }, { mie: "Darkness" }, { mie: "DHeights" },
-  { mie: "Spiders" }, { mie: "Snakes" }, { mie: "Rats" }, { mie: "Ageing" }, { mie: "Dangerous dogs" },
-  { mie: "Fear of public speaking" }];
+    { pas: 'Art exhibitions' }, { pas: 'Religion' }, { pas: 'Countryside, outdoors' }, { pas: 'Dancing' }
+    , { pas: 'Musical instruments' }, { pas: 'Writing' }, { pas: 'Passive sport' }, { pas: 'Active sport' }
+    , { pas: 'Gardening' }, { pas: 'Celebrities' }, { pas: 'Celebrities' }, { pas: 'Shopping' }
+    , { pas: 'Science and technology' }, { pas: 'Theatre' }, { pas: 'Fun with friends' }, { pas: 'Adrenaline sports' }
+    , { pas: 'Pets' }, {pas: 'cars'}];
+  miedos = [{ mie: 'Flying' }, { mie: 'Storm' }, { mie: 'Darkness' }, { mie: 'Heights' },
+  { mie: 'Spiders' }, { mie: 'Snakes' }, { mie: 'Rats' }, { mie: 'Ageing' }, { mie: 'Dangerous dogs' },
+  { mie: 'Fear of public speaking' }];
 
-  frases = [{ fra: "Healthy eating" }, { fra: "Daily events" }, { fra: "Prioritising workload" }, { fra: "Writing notes" },
-    { fra: "Workaholism" }, { fra: "Thinking ahead" }, { fra: "Final judgement" }, { fra: "Reliability" }];
+  frases = [{ fra: 'Healthy eating' }, { fra: 'Daily events' }, { fra: 'Prioritising workload' }, { fra: 'Writing notes' },
+    { fra: 'Workaholism' }, { fra: 'Thinking ahead' }, { fra: 'Final judgement' }, { fra: 'Reliability' }];
 
-  frases2 = [{ fra2: "Keeping promises" }, { fra2: "Loss of interest" }, { fra2: "Friends versus money" }, { fra2: "Funniness" },
-    { fra2: "Fake" }, { fra2: "Criminal damage" }, { fra2: "Decision making" }, { fra2: "Elections" }];
+  frases2 = [{ fra2: 'Keeping promises' }, { fra2: 'Loss of interest' }, { fra2: 'Friends versus money' }, { fra2: 'Funniness' },
+    { fra2: 'Fake' }, { fra2: 'Criminal damage' }, { fra2: 'Decision making' }, { fra2: 'Elections' }];
 
-  frases3 = [{ fra3: "Self-criticism" }, { fra3: "Judgement calls" }, { fra3: "Hypochondria" }, { fra3: "Empathy" },
-    { fra3: "Eating to survive" }, { fra3: "Giving" }, { fra3: "Compassion to animals" }, { fra3: "Borrowed stuff" }];
+  frases3 = [{ fra3: 'Self-criticism' }, { fra3: 'Judgement calls' }, { fra3: 'Hypochondria' }, { fra3: 'Empathy' },
+    { fra3: 'Eating to survive' }, { fra3: 'Giving' }, { fra3: 'Compassion to animals' }, { fra3: 'Borrowed stuff' }];
 
-  frases4 = [{ fra4: "Loneliness" }, { fra4: "Cheating in school" }, { fra4: "Health" }, { fra4: "Changing the past" },
-    { fra4: "God" }, { fra4: "Dreams" }, { fra4: "Charity" }, { fra4: "Number of friends" }];
+  frases4 = [{ fra4: 'Loneliness' }, { fra4: 'Cheating in school' }, { fra4: 'Health' }, { fra4: 'Changing the past' },
+    { fra4: 'God' }, { fra4: 'Dreams' }, { fra4: 'Charity' }, { fra4: 'Number of friends' }];
 
-  importantes = [{ imp: "Waiting" }, { imp: "New environmentg" }, { imp: "Mood swings" }, { imp: "Appearance and gestures" }, { imp: "Socializing" }, 
-    { imp: "Achievements" }, { imp: "Responding to a serioues letter" }];
+  importantes = [{ imp: 'Waiting' }, { imp: 'New environmentg' }, { imp: 'Mood swings' }, { imp: 'Appearance and gestures' }, { imp: 'Socializing' },
+    { imp: 'Achievements' }, { imp: 'Responding to a serioues letter' }];
 
-  importantes2 = [{ imp: "Children" }, { imp: "Assertiveness" }, { imp: "Getting angry" }, { imp: "Knowing the right people" }, { imp: "Public speaking" },
-    { imp: "Unpopularity" }, { imp: "Life struggles" }];
+  importantes2 = [{ imp: 'Children' }, { imp: 'Assertiveness' }, { imp: 'Getting angry' }, { imp: 'Knowing the right people' }, { imp: 'Public speaking' },
+    { imp: 'Unpopularity' }, { imp: 'Life struggles' }];
 
-  importantes3 = [{ imp: "Happiness in life" }, { imp: "Energy levels" }, { imp: "Small dogs" }, { imp: "  Big dogs" }, { imp: "Personality" },
-    { imp: "Finding lost valuables" }, { imp: "Getting up" }, { imp: "Interests or hobbies" }, { imp: "Parent's advice" }, { imp: "Questionnaires or polls" }];
-  
+  importantes3 = [{ imp: 'Happiness in life' }, { imp: 'Energy levels' }, { imp: 'Small dogs' }, { imp: '  Big dogs' }, { imp: 'Personality' },
+    { imp: 'Finding lost valuables' }, { imp: 'Getting up' }, { imp: 'Interests or hobbies' }, { imp: 'Parent\'s advice' }, { imp: 'Questionnaires or polls' }];
+
 
   cuest = {
     numMusica1: 1,
@@ -61,20 +61,25 @@ export class CuestionarioComponent implements OnInit {
     frase16: [],
     puntual17: 'i am always on time',
     mientes18: 'never',
-    importante19:[],
-    importante20:[],
-    importante21:[],
-    internet22:'less than an hour a day'
-  }
+    importante19: [],
+    importante20: [],
+    importante21: [],
+    internet22: 'less than an hour a day'
+  };
 
   constructor() { }
-  
+
   print() {
     console.log(this.cuest);
   }
 
 
   ngOnInit() {
+  }
+
+  send(){
+    console.log(this.cuest);
+    alert('Enviado');
   }
 
 }

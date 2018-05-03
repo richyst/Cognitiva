820c47b1151c6ca6bc3c55c4c0636036af1f2b66
richyst <ricardo_1994@hotmail.com>
2018-05-02 23:15:04 -0500

    Fix: servicio para deployment

--- src/app/services/cuestionario.service.ts
+++ src/app/services/cuestionario.service.ts
@@ -6,7 +6,7 @@ import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class CuestionarioService {
-  url = 'http://localhost:5000/';
+  url = 'http://127.0.0.1:5000/';
   constructor(private _http: Http) { }
 
   enviar(body) {

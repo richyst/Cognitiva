da4ebc6d0c0515edb987e02b4cdcb4d3abaf508c
richyst <ricardo_1994@hotmail.com>
2018-05-03 00:11:37 -0500

    Cambio de puerto

--- src/app/services/cuestionario.service.ts
+++ src/app/services/cuestionario.service.ts
@@ -6,7 +6,7 @@ import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class CuestionarioService {
-  url = 'http://127.0.0.1:5000/';
+  url = 'http://127.0.0.1:4000/';
   constructor(private _http: Http) { }
 
   enviar(body) {
